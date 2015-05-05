f72127e01ca07e4a8efcf31bb56d1008f65b904c
handsomezhou <quanjunzhou@gmail.com>
2015-05-06 01:07:35 +0800

    add phone info& setting&about to more view

--- src/com/handsomezhou/mobileassistant/fragment/MoreFragment.java
+++ src/com/handsomezhou/mobileassistant/fragment/MoreFragment.java
@@ -1,41 +1,103 @@
 package com.handsomezhou.mobileassistant.fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import com.handsomezhou.mobileassistant.R;
+import com.handsomezhou.mobileassistant.adapter.MoreAdapter;
+import com.handsomezhou.mobileassistant.model.IconButtonData;
+import com.handsomezhou.mobileassistant.model.IconButtonValue;
 
 public class MoreFragment extends BaseFragment {
-	private Button mMoreBtn;
+	private List<IconButtonData> mMoreData;
+	private GridView mMoreGv;
+	private MoreAdapter mMoreAdapter;
+	
+	public enum MORE_TAG{
+		PHONE_INFO,
+		SETTING,
+		ABOUT,
+		
+	}
+	
 	@Override
 	protected void initData() {
 		setContext(getActivity().getApplicationContext());
-
+		mMoreData=new ArrayList<IconButtonData>();
+		
+		/*Start : phone info*/
+		IconButtonValue phoneInfoIconButtonValue=new IconButtonValue(MORE_TAG.PHONE_INFO, R.drawable.phone_info, R.string.phone_info);
+		IconButtonData phoneInfoIconButtonData=new IconButtonData(getContext(), phoneInfoIconButtonValue);
+		mMoreData.add(phoneInfoIconButtonData);
+		/*End : phone info*/
+		
+		/*Start : setting*/
+		IconButtonValue settingIconButtonValue=new IconButtonValue(MORE_TAG.SETTING, R.drawable.setting, R.string.setting);
+		IconButtonData settingIconButtonData=new IconButtonData(getContext(),settingIconButtonValue);
+		mMoreData.add(settingIconButtonData);
+		/*End : setting*/
+		
+		/*Start : about*/
+		IconButtonValue aboutIconButtonValue=new IconButtonValue(MORE_TAG.ABOUT, R.drawable.about, R.string.about);
+		IconButtonData aboutIconButtonData=new IconButtonData(getContext(), aboutIconButtonValue);
+		mMoreData.add(aboutIconButtonData);
+		/*End : PHONE_INFO*/
 	}
 
 	@Override
 	protected View initView(LayoutInflater inflater, ViewGroup container) {
 		View view=inflater.inflate(R.layout.fragment_more, container, false);
-		mMoreBtn=(Button) view.findViewById(R.id.more_btn);
+		mMoreGv=(GridView)view.findViewById(R.id.more_grid_view);
+		mMoreAdapter=new MoreAdapter(getContext(), R.layout.icon_button_grid_item, mMoreData);
+		mMoreGv.setAdapter(mMoreAdapter);
+		
 		return view;
 	}
 
 	@Override
 	protected void initListener() {
-		if(null!=mMoreBtn){
-			mMoreBtn.setOnClickListener(new View.OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					Toast.makeText(getActivity().getApplicationContext(),getContext().getResources().getString(R.string.more) , Toast.LENGTH_SHORT).show();
-					
-				}
-			});
-		}
+		mMoreGv.setOnItemClickListener(new OnItemClickListener() {
 
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				IconButtonData iconButtonData = mMoreData.get(position);
+				viewSelect(iconButtonData);
+				
+			}
+		});
 	}
 
+	 private void viewSelect(IconButtonData iconButtonData) {
+	        if (null == iconButtonData) {
+	            return;
+	        }
+
+		switch ((MORE_TAG) iconButtonData.getIconButtonValue().getTag()) {
+
+		case PHONE_INFO:
+
+			break;
+		case SETTING:
+
+			break;
+		case ABOUT:
+
+			break;
+
+		default:
+			break;
+		}
+
+	        Toast.makeText(getContext(), iconButtonData.getIconButtonValue().getTag().toString(),
+	                Toast.LENGTH_SHORT).show();
+	    }
 }
